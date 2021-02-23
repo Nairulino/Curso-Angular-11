cadb940534b783da41e64cd14a5c241d533569db
Antonio Albaladejo Romero <antonio.95.29@gmail.com>
2021-02-23 12:26:37 +0100

    Ficheros de test modificados

--- curso/src/app/app.component.spec.ts
+++ curso/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'curso'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso');
+    expect(app.title).toEqual('Curso de Angular 11');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('curso app is running!');
+    expect(compiled.querySelector('.toolbar span').textContent).toContain('Curso');
   });
 });
