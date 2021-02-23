e2372753588a4457169f9feee3b8341717b6bc0c
Antonio Albaladejo Romero <antonio.95.29@gmail.com>
2021-02-23 11:22:17 +0100

    Inicio de proyecto

---
+++ curso/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ind-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Curso de Angular 11';
+  logo = '../assets/img/logo_angular.svg'
+}
